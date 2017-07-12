e089ec6c4539e471e0ab7d192b273cb7c2f2750c
ipankajgarg <gargpankaj901@gmail.com>
2017-07-12 11:32:02 -0700

    Add files via upload

---
+++ route1.js
@@ -0,0 +1,16 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider,$locationProvider) {
+    $locationProvider.hashPrefix('');
+//    $routeProvider.when("/", {
+//        templateUrl : "index.html"
+//    })
+    $routeProvider.when("/Halanx", {
+        templateUrl : "index.html"
+    })
+    .when("/aboutus", {
+        templateUrl : "aboutus.html"
+    })
+    .when("/contactus", {
+        templateUrl : "contactus.html"
+    });
+});
